c6d8e901b8854315e7ac9806530af1a3de027c8b
cod-rivers <cod-rivers@Meulinux.myguest.virtualbox.org>
2024-02-10 23:46:11 -0300

    10/02/2024

---
+++ exercicios_em_c/ft_list_size/ft_list_size.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+#include "ft_list.h"
+
+int	ft_list_size(t_list *begin_list)
+{
+	if (begin_list == 0)
+		return (0);
+	else
+		return (1 + ft_list_size (begin_list -> next));
+}
+
+/*#include <stdio.h>
+#include <stdlib.h>
+
+int	main(void)
+{
+	int n = 0;
+
+	t_list *c = malloc(sizeof(*c));
+	c->next = 0;
+	c->data = &n;
+
+	t_list *b = malloc(sizeof(*b));
+	b->next = c;
+	b->data = &n;
+
+	t_list *a = malloc(sizeof(*a));
+	a->next = b;
+	a->data = &n;
+
+	printf("%d\n", ft_list_size(a));
+}*/
